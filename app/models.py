0074b7f56ece0dd4c76c53be6835a39de936b591
jrco1989 <jrco1989@hotmail.com>
2022-06-07 07:02:49 -0500

    avances

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Comments(models.Model):
+    email = models.CharField(max_length=40)
+    name =  models.CharField(max_length=100)
+    comment =  models.TextField()
+    ESTADO = (
+        (True, 'Resuelto'),
+        (False, 'Pendiente'),
+    )
+    
+    estado = models.BooleanField(
+        choices=ESTADO,
+        default=False,
+    )
+
+    class Meta:
+        verbose_name_plural = 'comentarios'
--- app/views.py
+++ app/views.py
@@ -1,10 +1,28 @@
+import email
 from django.shortcuts import render
+from app.models import Comments
+from django.contrib import messages
+
 
 def home(request):
-    print(request.GET)
+    print(request)
     print("queryset")
     
+    if "POST"==request.method:
+        print("POSTFSDFSDF")
+    #    import pdb; pdb.set_trace()
+        nombre = request.POST['nombre']
+        correo = request.POST['correo']
+        comentario = request.POST['comentario']
+        Comments.objects.create(
+            email = correo,
+            name =  nombre,
+            comment = comentario,
+        )
+        messages.success(request, "Mensaje enviado!")
+        
     return render(
         request, 
         './home.html'
+         
     )
